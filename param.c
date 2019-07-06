74ebe576578383e0e2ee577f18dad5bee6ffbfcc
Beef Jesse <bjesse@oa-a1.21-school.ru>
2019-07-06 18:37:24 +0300

    add oaram

--- param.c
+++ param.c
@@ -1,18 +1,38 @@
 #include "ft_printf.h"
 
 
-int add_param(t_param *param);
+int add_param(t_param **param, char **format)
+{
+	t_param *now_param;
+	if (*(*format + 1) == '%')
+		return (0);
+	now_param = add_param_list(param);
+
+
+	parse_param(*format, now_param);
+
+	
+	return (0);
+}
+
+
+
+
+
+
 
 
 int read_params(t_param **param, char *format)
 {
-	t_param *last_param;
+	// t_param *last_param;
 
 	while (*format)
 	{
 		while (*format && *format != '%')
 			format++;
 		if (*format == '%')
-			format = add_param(param)
-
+			if(add_param(param, &format))
+				return (1);
+	}
+	return(0);
 }
