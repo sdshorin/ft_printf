6f4f0a75bd70d4a91115cb2cb35ea9a6110f1514
Shorin Sergey <serres123@ya.ru>
2019-07-19 20:24:51 +0300

    0

--- print_csp.c
+++ print_csp.c
@@ -2,8 +2,32 @@
 #include "ft_printf.h"
 
 
+int print_with_flug(char *str, t_param *param)
+{
+	int str_len;
+	char fill_char;
 
+	fill_char = ' ';
+	if (param->zero)
+		fill_char = '0';
+	str_len = ft_min(ft_strlen(str), param->precision);
+	if (param->minimum_size > str_len)
+		if (param->minus)
+		{
+			write(1, str, str_len);
+			ft_write_char_many(fill_char, param->minimum_size - str_len);
+		}
+		else
+		{
+			ft_write_char_many(fill_char, param->minimum_size - str_len);
+			write(1, str, str_len);
+		}
+	else
+		write(1, str, str_len);
+	
+	return ();
 
+}
 
 
 int print_char(t_param **param, va_list ap)
@@ -23,8 +47,7 @@ int  print_str(t_param **param, va_list ap)
 	int str_size;
 
 	str = va_arg(ap, char*);
-	str_size = ft_strlen(str);
-	write(1, str, str_size);
+	str_size = print_with_flug(str, *param);
 	return (str_size);
 }
 
